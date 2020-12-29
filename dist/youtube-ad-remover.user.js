bbe200a36ae7dd6b777daeb7bd368ba99e0b6695
Hung Dinh <dinhhung2910@gmail.com>
2020-12-29 11:41:00 +0700

    * [Youtube-ad-remover] Fix error when skip ads

--- dist/youtube-ad-remover.user.js
+++ dist/youtube-ad-remover.user.js
@@ -20,4 +20,4 @@
 // @grant       GM_log
 // ==/UserScript==
 
-(()=>{"use strict";const e=e=>{try{e.parentElement.removeChild(e)}catch(e){console.error(e)}},t=[".ytp-ad-image-overlay","ytd-player-legacy-desktop-watch-ads-renderer","#masthead-ad"],r=[".ytp-ad-skip-button"],a=()=>{r.forEach((t=>{document.querySelectorAll(t).forEach((t=>{e(t)}))}))};setInterval((()=>{t.forEach((t=>{document.querySelectorAll(t).forEach((t=>{e(t)}))}))}),400),setInterval(a,400)})();
+(()=>{"use strict";const e=[".ytp-ad-image-overlay","ytd-player-legacy-desktop-watch-ads-renderer","#masthead-ad"],t=[".ytp-ad-skip-button"],r=()=>{t.forEach((e=>{document.querySelectorAll(e).forEach((e=>{e.click()}))}))};setInterval((()=>{e.forEach((e=>{document.querySelectorAll(e).forEach((e=>{(e=>{try{e.parentElement.removeChild(e)}catch(e){console.error(e)}})(e)}))}))}),400),setInterval(r,400)})();
--- src/youtube-ad-remover/skipVideoAds.js
+++ src/youtube-ad-remover/skipVideoAds.js
@@ -1,10 +1,9 @@
-import {removeDOM} from '../common/util';
 import {INTERVAL_TIME, SKIP_AD_LABELS} from './constants';
 
 export const skipVideoAds = () => {
   SKIP_AD_LABELS.forEach((label) => {
     document.querySelectorAll(label).forEach((elm) => {
-      removeDOM(elm);
+      elm.click();
     });
   });
 };
