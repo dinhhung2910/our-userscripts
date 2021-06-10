e8cdb094da30b634c107b0fc8054d20105a55fb9
Hung Dinh <dinhhung2910@gmail.com>
2021-06-10 20:43:34 +0700

    * Update youtube static ads remover

--- src/youtube-ad-remover/constants.js
+++ src/youtube-ad-remover/constants.js
@@ -2,6 +2,7 @@ export const AD_LABELS = [
   '.ytp-ad-image-overlay',
   'ytd-player-legacy-desktop-watch-ads-renderer',
   '#masthead-ad',
+  'ytd-promoted-sparkles-web-renderer',
 ];
 
 export const SKIP_AD_LABELS = [
