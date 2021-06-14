772b79df227a82e1d23046dfcfce31380db4d66c
Hung Dinh <dinhhung2910@gmail.com>
2021-06-14 09:36:05 +0700

    * update version

--- dist/facebook-ad-remover.meta.js
+++ dist/facebook-ad-remover.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Facebook advertisements remover
-// @version     1.2.0
+// @version     1.2.1
 // @author      Hung Dinh
 // @description Remove advertisements on facebook web
 // @homepage    https://github.com/dinhhung2910/our-userscripts#readme
