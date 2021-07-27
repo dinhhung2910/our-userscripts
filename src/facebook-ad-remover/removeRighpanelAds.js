fc919e8d10e987ab263b7d40ce70dd94c1ac08f3
Hung Dinh <dinhhung2910@gmail.com>
2021-07-27 21:29:52 +0700

    * [Facebook] Remove right-panel ads even if it is not located at first position

--- src/facebook-ad-remover/removeRighpanelAds.js
+++ src/facebook-ad-remover/removeRighpanelAds.js
@@ -1,32 +1,46 @@
 const {checkContainSelectors, removeDOM} = require('../common/util');
-const {RIGHT_PANEL_SELECTOR, RIGHT_PANEL_ADS_ARIA, AD_LABELS} = require('./constants');
+const {
+  RIGHT_PANEL_SELECTOR,
+  RIGHT_PANEL_ADS_ARIA,
+  AD_LABELS,
+} = require('./constants');
+
+const removeAdsection = (adSection) => {
+  if (checkContainSelectors(adSection, RIGHT_PANEL_ADS_ARIA)) {
+    removeDOM(adSection);
+    console.log('Removed right panel ads');
+    return true;
+  } else {
+    let isAd = false;
+    const adContent = adSection.innerText;
+
+    AD_LABELS.forEach((label) => {
+      if (adContent.includes(label)) {
+        isAd = true;
+      }
+    });
+
+    if (isAd) {
+      removeDOM(adSection);
+    }
+    return isAd;
+  }
+};
 
 const removeRightpanelAds = () => {
   const rightRale = document.querySelector(RIGHT_PANEL_SELECTOR);
   if (rightRale) {
     setTimeout(() => {
-      let adSection = null;
-      try {
-        adSection = rightRale.firstElementChild.firstElementChild;
-      } catch (e) {
-        // mute
-      }
+      const childList = (rightRale.firstElementChild &&
+       rightRale.firstElementChild.children) || [];
 
-      if (checkContainSelectors(adSection, RIGHT_PANEL_ADS_ARIA)) {
-        removeDOM(adSection);
-        console.log('Removed right panel ads');
-      } else {
-        let isAd = false;
-        const adContent = adSection.innerText;
-
-        AD_LABELS.forEach((label) => {
-          if (adContent.includes(label)) {
-            isAd = true;
-          }
-        });
-
-        if (isAd) {
-          removeDOM(adSection);
+      for (let i = 0; i < childList.length; i++) {
+        let adSection = null;
+        try {
+          adSection = childList[i];
+          removeAdsection(adSection);
+        } catch (e) {
+          // mute
         }
       }
     }, 0);
